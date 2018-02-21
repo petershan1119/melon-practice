fd6e7803e3c2d532dce87521ab119fac983ee2b5
Sang Won Han <peter.s.han.1119@gmail.com>
2018-02-21 18:27:57 +0900

    artist app 반쯤 완성

---
+++ django/artist/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'artist'
+
+urlpatterns = [
+    path('', views.artist_list, name='artist-list'),
+    path('add/', views.artist_add, name='artist-add'),
+]
---
+++ django/artist/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+
+from .models import Artist
+
+def artist_list(request):
+    artists = Artist.objects.all()
+    context = {
+        'artists': artists,
+    }
+    return render(request, 'artist/artist_list.html', context)
+
+
+def artist_add(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        Artist.objects.create(
+            name=name,
+        )
+        return redirect('artist:artist-list')
+    else:
+        return render(request, 'artist/artist_add.html')
