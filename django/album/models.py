89e1c337a1a40bbad32fcab3cbb7fbd15d584d2c
Sang Won Han <peter.s.han.1119@gmail.com>
2018-02-22 12:05:34 +0900

    Song에 artists정보 update

--- django/album/models.py
+++ django/album/models.py
@@ -13,18 +13,20 @@ class Album(models.Model):
         upload_to='album',
         blank=True,
     )
-    artists = models.ManyToManyField(
-        Artist,
-        verbose_name='아티스트 목록',
-    )
+    # artists = models.ManyToManyField(
+    #     Artist,
+    #     verbose_name='아티스트 목록',
+    # )
     release_date = models.DateField()
 
     @property
     def genre(self):
         return ', '.join(self.song_set.values_list('genre', flat=True)).distinct()
 
+    # def __str__(self):
+    #     return '{title} [{artists}]'.format(
+    #         title=self.title,
+    #         artists=', '.join(self.artists.values_list('name', flat=True)),
+    #     )
     def __str__(self):
-        return '{title} [{artists}]'.format(
-            title=self.title,
-            artists=', '.join(self.artists.values_list('name', flat=True)),
-        )
+        return self.title
