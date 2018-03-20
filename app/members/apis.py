e931519dae1e5cca78ad7dd08447e81350551f86
Sang Won Han <peter.s.han.1119@gmail.com>
2018-03-20 14:08:58 +0900

    AuthTokenView 완선

--- app/members/apis.py
+++ app/members/apis.py
@@ -13,6 +13,19 @@ class AuthTokenView(APIView):
         # 유저 인증에 성공했을 경우
         # 토큰을 생성하거나 있으면 존재하는 걸 가져와서
         # Response로 돌려줌
+
+        # username = request.data.get('username')
+        # password = request.data.get('password')
+        # user = authenticate(username=username, password=password)
+        # if user is not None:
+        #     token, _ = Token.objects.get_or_create(user=user)
+        #     data = {
+        #         'token': token.key
+        #     }
+        #     return Response(data)
+        # raise APIException('authenticate failure')
+        # raise AuthenticationFailed()
+
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
